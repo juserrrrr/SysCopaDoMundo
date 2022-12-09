c644e397d6cc1eaf83ee14a6dec73e2bbf1380de
JoaoVAC <jvalves62@outlook.com>
2022-12-09 18:26:37 -0300

    Criação da tela de arbitros e suas funcionalidades

---
+++ CopaDoMundoApp/src/application/controller/DialogArbitroController.java
@@ -0,0 +1,87 @@
+package application.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import application.model.Arbitro;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+public class DialogArbitroController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+    
+    @FXML
+    private TextField campoNome;
+
+    private Stage stage;
+    
+	private ObservableList<Arbitro> arbitroData;
+	
+	private TableView<Arbitro> arbitroTableView;
+	
+	private Arbitro arbitro;
+
+	public void setCampoNome(String nome) {
+		this.campoNome.setText(nome);
+	}
+    
+    public Stage getStage() {
+		return stage;
+	}
+
+	public void setStage(Stage stage) {
+		this.stage = stage;
+	}
+	
+	public ObservableList<Arbitro> getarbitroData() {
+		return arbitroData;
+	}
+	
+	public void setArbitroData(ObservableList<Arbitro> arbitroData) {
+		this.arbitroData = arbitroData;
+	}
+
+	public void setArbitroTableView(TableView<Arbitro> arbitroTableView) {
+		this.arbitroTableView = arbitroTableView;
+	}
+	
+	public void setArbitro(Arbitro arbitro) {
+		this.arbitro = arbitro;
+	}
+	
+	@FXML
+    void cancelarCriacao(MouseEvent event) {
+    	this.stage.close();
+    }
+	
+	private void criarArbitro() {
+		Arbitro selec = new Arbitro(campoNome.getText());
+		Arbitro.arbitroDao.create(selec);
+		arbitroData.add(selec);
+	}
+		
+	private void editarArbitro() {
+		arbitro.setNome(campoNome.getText());
+	}
+			
+	
+	@FXML
+    void confirmarCriacao(MouseEvent event) {
+		if(arbitro == null) {
+			criarArbitro();
+		} else {
+			editarArbitro();
+		}
+		arbitroTableView.refresh();
+    	stage.close();
+    }
+}
