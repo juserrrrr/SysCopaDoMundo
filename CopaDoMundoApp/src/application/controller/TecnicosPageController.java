18623d9cf3fbc0b79bb54734a5e95ef84168522f
José Gabriel <89746639+juserrrrr@users.noreply.github.com>
2022-12-09 18:46:33 -0300

    Tirando import

--- CopaDoMundoApp/src/application/controller/TecnicosPageController.java
+++ CopaDoMundoApp/src/application/controller/TecnicosPageController.java
@@ -5,7 +5,6 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.ResourceBundle;
 
-import application.model.Jogador;
 import application.model.Selecao;
 import application.model.Tecnico;
 import javafx.collections.FXCollections;
